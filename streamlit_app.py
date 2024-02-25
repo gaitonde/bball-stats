9dd0d8d80af6b3fbc1c6424e49ef3f429db1e1df
dayal <dayal@commerznxt.com>
2024-02-25 15:42:14 -0800

    tweaks

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 #import pandas as pd
 #import numpy as np
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(
+    worksheet="Data",
+)
+# Print results.
+# for row in df.itertuples():
+#     st.write(f"{row.name} has a :{row.pet}:")
 
 def calc_effective_fg_pct(fgm, fgm3, fga):
     return (fgm + (0.5*fgm3))/fga
@@ -8,4 +17,13 @@ def calc_effective_fg_pct(fgm, fgm3, fga):
 pct = calc_effective_fg_pct(5,3,14)
 
 st.title('Simple Stat Tracker')
+number = st.number_input('Insert a number')
+st.write('The current number is ', number)
 st.write(pct)
+
+st.button("Reset", type="primary")
+
+if st.button('Say hellox'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
