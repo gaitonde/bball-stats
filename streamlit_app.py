3cd1bcc3a843e4ea00e334112e75e9bf59d05948
Dayal Gaitonde <gaitonde@users.noreply.github.com>
2024-02-25 10:52:05 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Simple Stat Tracker')
+st.title('Simple Stat Tracker: ' + calc_effective_fg_pct(5,3,14))
+
+def calc_effective_fg_pct(fgm, fgm3, fga):
+    return (fgm + (0.5*fgm3))/fga
