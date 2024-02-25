f911859dfa886805e3312f987fae701b695b9add
dayal <dayal@commerznxt.com>
2024-02-25 11:00:32 -0800

    tweak

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
 
-def calc_effective_fg_pct(fgm, fgm3, fga):
-    return (fgm + (0.5*fgm3))/fga
+# def calc_effective_fg_pct(fgm, fgm3, fga):
+#     return (fgm + (0.5*fgm3))/fga
 
-pct = calc_effective_fg_pct(5,3,14)
+# pct = calc_effective_fg_pct(5,3,14)
 
-st.title('Simple Stat Tracker' )
-st.write(pct)
+st.title('Simple Stat Trackerx')
+# st.write(pct)
